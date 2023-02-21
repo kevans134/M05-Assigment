72cea307fa7a5f84be0003a8a6b976b1c70ad0c6
kevans134 <91567352+kevans134@users.noreply.github.com>
2023-02-20 23:43:34 -0500

    Add files via upload

---
+++ project/test.py
@@ -0,0 +1,32 @@
+import unittest
+from fractions import Fraction
+
+from my_sum import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+    
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+    #after running the unittest result show that one command pass and one fail./
+    # One say FAIL and given the line of code which is line 16. 
+    # Mean that the fraction does not equal to one.
